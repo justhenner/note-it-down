16d4d66010231c6b03cb9c44950467ce44adb17f
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-09 22:37:45 -0500

    set uuid

--- Develop/server.js
+++ Develop/server.js
@@ -17,9 +17,9 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
