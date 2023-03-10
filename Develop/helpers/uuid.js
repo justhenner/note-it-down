8cfd187ea429d951c266dd5d213c2695275f3551
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-10 16:35:18 -0500

    uuid.js modified

--- Develop/helpers/uuid.js
+++ Develop/helpers/uuid.js
@@ -1,4 +1,4 @@
 module.exports = () =>
-    Math.floor((1+ Math.random()) * 0x10000)
-        .toString(16);
+    Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
         .subString(1);
